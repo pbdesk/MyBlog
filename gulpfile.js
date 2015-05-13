3e87ef50efd3d9063dd73490192e731f71532a5c
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2015-05-13 14:57:19 -0400

    Complete Build

--- gulpfile.js
+++ gulpfile.js
@@ -24,13 +24,38 @@
             .pipe(vinylPaths(GU.del));
     })
 
+    GU.gulp.task('clean:site', function(done){
+        return GU.gulp
+            .src(GU.Configs.appFolder + '_site')
+            .pipe(vinylPaths(GU.del));
+    })
+
     GU.gulp.task('minify-html', function(done){
         return GU.gulp
             .src(GU.Configs.htmlFiles)
             .pipe(GU.GP.minifyHtml({empty: true}))
             .pipe(GU.gulp.dest(GU.Configs.appFolder + '_site/'));
+    });
+
+    GU.gulp.task('jekyll', function (gulpCallBack){
+        var spawn = require('child_process').spawn;
+        var jekyll = spawn('jekyll', ['build', '--source=./app', '--destination=./app/_site'], {stdio: 'inherit'});
+
+        jekyll.on('exit', function(code) {
+            gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+        });
+    });
+
+    GU.gulp.task('BUILD-FULL', function(callback){
+        runSequence(
+            ['clean:build', 'clean:site'],
+            ['optimize-css', 'optimize-theme-css', 'optimize-js'],
+            ['bundle-css','bundle-js'],
+            'jekyll',
+            ['minify-html', 'optimize-images'],
+            callback
+        );
+
     })
-
-
 })();
 
